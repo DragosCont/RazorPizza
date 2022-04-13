55429278a92ac434d93381bb4822163678fa52b6
Cont Dragos <dragoscont@live.com>
2022-04-13 14:29:21 +0300

    added OnPost method to CustomPizza Model

--- RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,25 @@ namespace RazorPizza.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 3;
+            if (Pizza.Mushrooom) PizzaPrice += 3;
+            if (Pizza.Tuna) PizzaPrice += 5;
+            if (Pizza.Pineapple) PizzaPrice += 8;
+            if (Pizza.Ham) PizzaPrice += 2;
+            if (Pizza.Beef) PizzaPrice += 2;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PizzaName, PizzaPrice});
         }
     }
 }
