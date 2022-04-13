abd7eb9b5cc9433dd2991191de7699d5e4b50f70
Cont Dragos <dragoscont@live.com>
2022-04-13 15:53:40 +0300

    designed the Checkout page

--- RazorPizza/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizza.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            //sets the default name of pizza and the default image
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "CustomPizza";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
         }
     }
 }
