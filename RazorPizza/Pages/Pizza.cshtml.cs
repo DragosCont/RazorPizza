55898032d2db7e328986af4d9e64d65f4b501b33
Cont Dragos <dragoscont@live.com>
2022-04-13 17:09:12 +0300

    sets a mock db an the asp-route to diplay the details corectly

--- RazorPizza/Pages/Pizza.cshtml.cs
+++ RazorPizza/Pages/Pizza.cshtml.cs
@@ -1,10 +1,68 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Models;
 
 namespace RazorPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<PizzasModel> fakePizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel(){
+                ImageTitle="Margerita",
+                PizzaName="Margerita",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                FinalPrice=4},
+
+            new PizzasModel(){
+                ImageTitle="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                Beef=true,
+                FinalPrice=6 },
+
+            new PizzasModel(){
+                ImageTitle="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                Ham=true,
+                Mushrooom=true,
+                FinalPrice=9 },
+
+            new PizzasModel(){
+                ImageTitle="Hawaiian",
+                PizzaName="Hawaiian",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                Beef=true,
+                Pineapple=true,
+                FinalPrice=14 },
+
+            new PizzasModel(){
+                ImageTitle="Pepperoni",
+                PizzaName="Pepperoni",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                Peperoni=true,
+                FinalPrice=7 },
+
+            new PizzasModel(){
+                ImageTitle="Seafood",
+                PizzaName="Seafood",
+                BasePrice=2,
+                TomatoSauce=true,
+                Cheese=true,
+                Tuna=true,
+                FinalPrice=9 }
+        };
         public void OnGet()
         {
         }
