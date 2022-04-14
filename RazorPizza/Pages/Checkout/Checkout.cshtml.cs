cfd0fff4c1acd591d647975d3743c7786f4920f9
Cont Dragos <dragoscont@live.com>
2022-04-14 12:30:46 +0300

    added the DB to the code and save the orders into DB

--- RazorPizza/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizza/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizza.Data;
+using RazorPizza.Models;
 
 namespace RazorPizza.Pages.Checkout
 {
@@ -9,6 +11,14 @@ namespace RazorPizza.Pages.Checkout
         public string PizzaName { get; set; }
         public float PizzaPrice { get; set; }
         public string ImageTitle { get; set; }
+
+        public readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            //this is the DB
+            _context = context;
+        }
+
         public void OnGet()
         {
             //sets the default name of pizza and the default image
@@ -20,6 +30,16 @@ namespace RazorPizza.Pages.Checkout
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            //adds the order into the DB
+            _context.PizzaOrders.Add(pizzaOrder);
+
+            //save the DB
+            _context.SaveChanges();
 
         }
     }
